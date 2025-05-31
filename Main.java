43af2bc9b2a013c152c5280aeae51a1b9d4b1459
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-30 20:02:18 -0500

    added questions and function to display them from Main method using object reference.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main{
     public static void main(String args[]){
         // System.out.println("This is main class");
+        QuestionService service = new QuestionService();
+        service.displayQuestions();
     }
 }
--- QuestionService.java
+++ QuestionService.java
@@ -1,3 +1,20 @@
 public class QuestionService {
-    
+    Question[] questions = new Question[5];
+
+    public QuestionService(){
+        questions[0] = new Question(1, "Wrapper class of int is", "Integer", "Character", "Float", "String", "Integer");
+        questions[1] = new Question(2, "Wrapper class of char is", "Integer", "Character", "Float", "String", "Character");
+        questions[2] = new Question(3, "Wrapper class of float is", "Integer", "Character", "Float", "String", "Float");
+        questions[3] = new Question(4, "Wrapper class of double is", "Integer", "Character", "Float", "Double", "Double");
+        questions[4] = new Question(5, "Wrapper class of long is", "Integer", "Character", "Long", "String", "Long");
+    }
+
+    public void displayQuestions(){
+        for(Question q : questions){
+            System.out.println("Q"+q.getId() + ") " +q.getQuestion());
+            System.out.println("a. "+q.getOpt1()+"\tb. "+q.getOpt2() + "\tc. "+q.getOpt3() + "\td. "+q.getOpt4());
+            System.out.println("\n");
+        }
+    }
+
 }
